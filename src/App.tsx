e3ec8ae482112be390bc7387069a2547985206c7
The-click <ramzanmal04@mail.ru>
2025-07-12 01:07:28 +0300

    feat:init class component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { Component } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+class App extends Component {
+  state = { data: [] };
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  getData = (query: string) => {
+    this.setState({
+      data: [
+        { name: 'Blabla', description: '123' },
+        { name: 'Qas', description: 'Zxc' },
+      ],
+    });
+  };
+
+  render() {
+    return <h1>Hello, World!</h1>;
+  }
 }
 
-export default App
+export default App;
